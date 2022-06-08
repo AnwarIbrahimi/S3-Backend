e02187968dbaa7ff60511a852a5f93df36ab0e40
Anwar <465821@student.fontys.nl>
2022-06-08 11:32:52 +0200

    removed pom configuration

--- src/main/java/com/example/springpopcorntime/Services/AccountService.java
+++ src/main/java/com/example/springpopcorntime/Services/AccountService.java
@@ -1,12 +1,12 @@
 package com.example.springpopcorntime.Services;
 
 
-import com.example.springpopcorntime.Model.Account;
-import com.example.springpopcorntime.Model.AccountViewModel;
+//import com.example.springpopcorntime.Model.Account;
+//import com.example.springpopcorntime.Model.AccountViewModel;
 
-import java.util.List;
+//import java.util.List;
 
 public interface AccountService {
-    AccountViewModel login(String emailId);
-    public List<Account> getAllAccounts();
+    //AccountViewModel login(String emailId);
+    //public List<Account> getAllAccounts();
 }
